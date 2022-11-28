8b63c9a59968321260f6570cb639784882718b29
Marco Antonio Barro Tortelli <marcobarrotortelli@gmail.com>
2022-11-28 00:31:05 -0300

    menu 99% pronto

--- js/index.js
+++ js/index.js
@@ -2,5 +2,14 @@ const toggleButton = document.getElementsByClassName('navbar-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-menu')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-});
+        navbarLinks.classList.toggle('remove-menu')
+})
+
+function myFunction() {
+    if (window.matchMedia('(max-width: 768px)').matches) {
+        navbarLinks.classList.add('remove-menu');
+    } else {
+        navbarLinks.classList.add('add-menu');
+    }
+}
+window.addEventListener('window', myFunction());
